7d0b442c4bca98e9b15976099e6cda01eeb006b7
Michael Sanchez <michaelsanchez42@live.com>
2021-01-24 11:12:41 -0700

    added my name

--- api/server.js
+++ api/server.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const server = express();
 
 // Complete your server here!
 // Do NOT `server.listen()` inside this file!
-
+const myName = "michael";
 module.exports = server;
